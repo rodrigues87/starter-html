0125dbcb9779af5e825638b94b4aae2f3693ae93
Sala 19A <sala19a@ceines.com.br>
2022-05-05 20:51:34 -0300

    Initial commit

---
+++ javascript/acionamento/acionamento.js
@@ -0,0 +1,7 @@
+const idadePedro = 34;
+const idadeMaria = idadePedro * 2;
+
+let contador = 0;
+contador = contador +1;
+contador = contador +1;
+
---
+++ javascript/atividade/atividade1.js
@@ -0,0 +1,30 @@
+//VERIFIQUE SE UMA DETERMINADA PESSOA É MAIOR DE IDADE
+
+const idadePessoa = 24;
+
+exibirResultCheckIdade(idadePessoa);
+
+function exibirResultCheckIdade(idadePessoa) {
+    if(verificarMaiorIdade(idadePessoa)){
+        console.log("Essa pessoa é maior de 18 anos")
+    }else {
+        console.log("Essa pessoa é menor de 18 anos")
+    }
+}
+
+function verificarMaiorIdade(idadePessoa) {
+
+    validarIdade(idadePessoa);
+
+    if(idadePessoa >= 18){
+        return true;
+    }
+
+    return false;
+}
+
+function validarIdade(idadePessoa) {
+    if(idadePessoa === 0 ){
+        throw "Esta não é uma idade válida";
+    }
+}
